a62afc94331d9bd02c30a1b0bb43967cc7b67f39
raeKhan225 <raeesahkhan225@gmail.com>
2023-03-20 09:18:09 +0000

    README content added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Finding-Repetition-in-Genomes
+The finding repetition in genomes project will develop a web-based application
+for bioinformaticians so that they are able to find microsatellites in genomes.
+Genomes are stored as strings in FASTA files, using string manipulation
+bioinformaticians can analyse the genomes and are then able to make conclusions
+about the genomes that they've collected. Microsatellites (also known as Simple
+Sequence Repeats (SSRs)) are repetitive sections of DNA that repeat throughout
+the genome. Microsatellites are important for bioinformatics because they can be
+used to advance our understanding of biological systems, genetic diversity, and
+evolutionary processes. Furthermore, microsatellites are also useful for
+identifying and tracking genetic changes in populations, including those that may
+be important for human health or that are associated with diseases. Most
+commonly, Microsatellites are considered to be between 2 and 7 bases long.
--- web_microsat_finder/main.py
+++ web_microsat_finder/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask import request
 from werkzeug.utils import secure_filename
+from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__, template_folder='template')
 
@@ -14,9 +16,30 @@ def homepage():
 def upload_file():
     if request.method == 'POST':
         f = request.files['fastaFile']
-        f.save(
-            f"/Users/raeesahkhan/Documents/uni/Major_Project/web_microsat_finder/uploads/{secure_filename(f.filename)}")
-    return homepage()  # need to change to redirect to to  / path
+
+        # # https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application
+        # # Connect to the MongoDB client
+        # client = MongoClient('mongodb://localhost:27017/')
+        #
+        # # Select the database and collection
+        # db = client['mydatabase']
+        # collection = db['mycollection']
+        #
+        # # Define the data to insert into the database
+        # data = {
+        #     'timestamp': datetime.now(),
+        #     'project_title': 'Test Project',
+        #     'email_address': 'rak12@aber.ac.uk',
+        # }
+        #
+        # # Insert the data into the collection
+        # result = collection.insert_one(data)
+        #
+        # # Print the ID of the inserted document
+        # print("Document inserted with ID:", result.inserted_id)
+
+        f.save(f"/Users/raeesahkhan/Documents/uni/Major_Project/web_microsat_finder/uploads/{secure_filename(f.filename)}")
+    return homepage() # need to change to redirect to to  / path
 
 
 if __name__ == '__main__':
