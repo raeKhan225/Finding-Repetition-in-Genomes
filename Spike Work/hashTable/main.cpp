4c53c997025d2762d0ba9d862e1aaf54b27e23c0
raeKhan225 <raeesahkhan225@gmail.com>
2023-03-30 13:33:03 +0100

    Microsats found if repeats have 0 mismatch

--- Spike Work/hashTable/main.cpp
+++ Spike Work/hashTable/main.cpp
@@ -12,22 +12,23 @@ public:
      *
      * @param sequence
      * The sequence that the user inputs for microsatllite analysis
-     * @param minNoRepeats
+     * @param minLenRepeats
      * This is the minimum length of the repeats in the microsatellite
-     * @param maxNoRepeats
+     * @param maxLenRepeats
      * This is the maximum length of the repeats in the microsatellite
      * @param minLenMicrosat
      * This is the minimum length of repeats for the repeats to b considered a microsatellite
      * For example if the minimum length is 3 of repeat ATG then ATGATG will not but considered as a Microsatlite but ATGATGATG will be.
      */
 
-    void findMicrosats(string sequence, int minNoRepeats, int maxNoRepeats, int minLenMicrosat){
+    // need to deal with mismatches
+    void findMicrosat(string sequence, int minLenRepeats, int maxLenRepeats, int minLenMicrosat){
         // Get length of the sequence
         int lenOfSequence = sequence.length();
 
         // Go through each nucleotide in sequence as microsat can start from any position
-        for(int nucleotidePos = 0; nucleotidePos <= lenOfSequence - minNoRepeats; nucleotidePos ++){
-            for(int lenOfRepeats = minNoRepeats; lenOfRepeats <= maxNoRepeats; lenOfRepeats ++){
+        for(int nucleotidePos = 0; nucleotidePos <= lenOfSequence - minLenRepeats; nucleotidePos ++){
+            for(int lenOfRepeats = minLenRepeats; lenOfRepeats <= maxLenRepeats; lenOfRepeats ++){
                 // check if end of sequence
                 if(nucleotidePos + lenOfRepeats >= lenOfSequence){
                     break;
@@ -48,18 +49,50 @@ public:
                 // if repeats meet the threshold output the result
                 if(lenThreshold >= minLenMicrosat){
                     cout << sequence.substr(nucleotidePos, lenOfRepeats) << "   StartPos: " << startPos << "   EndPos: " << endPos << endl;
+                    addToHashtable(sequence.substr(nucleotidePos, lenOfRepeats), startPos, endPos);
                 }
             }
         }
-
+        printHashTable();
     }
 
+    // need to find which sequence it is in
+    void addToHashtable(string repeat, int startPos, int endPos){
+
+        // If repeat is not in hashtable - if micosat hasn't been found already
+        if(hashtable.find(repeat) == hashtable.end()){
+            hashtable[repeat] = {startPos, endPos};
+        }
+
+        // if microsat has already been found in another position
+        else {
+            vector<int> vec = hashtable[repeat];
+            // Updating the vector with additional start and stop positions
+            vec.push_back(startPos);
+            vec.push_back(endPos);
+            hashtable[repeat] = vec;
+        }
+    }
+
+    void printHashTable() {
+        // Printing of Unordered_MAP
+        cout << "HASHTABLE" << endl;
+        for (auto &[key, vec]: hashtable) {
+            cout << "Key: " << key << "    " << endl;
+            cout << "Start and end positions: ";
+            for (int i: vec) {
+                cout << i << "  ";
+            }
+            cout << "\n";
+        }
+    }
 };
 
 int main() {
+    // A repeat is the repeat in a micosat i.e. ATGATGATGATGATGATG the repeat = ATG
     string sequence;
-    int minNoRepeats;
-    int maxNoRepeats;
+    int minLenRepeats;
+    int maxLenRepeats;
     int minLenMicrosat;
 
     cout << "Enter the sequence: ";
@@ -68,15 +101,14 @@ int main() {
     cout << "Enter the minimum length of the Microsat: ";
     cin >> minLenMicrosat;
 
-    cout << "Enter the maximum number of repeats: ";
-    cin >> maxNoRepeats;
+    cout << "Enter the minimum length of repeats: ";
+    cin >> minLenRepeats;
 
-    cout << "Enter the minimum number of repeats: ";
-    cin >> minNoRepeats;
+    cout << "Enter the maximum length of repeats: ";
+    cin >> maxLenRepeats;
+
+
 
     MicrosatFinder microsatFinder;
-    microsatFinder.findMicrosats(sequence, minNoRepeats, maxNoRepeats, minLenMicrosat);
-
-
-
+    microsatFinder.findMicrosat(sequence, minLenRepeats, maxLenRepeats, minLenMicrosat);
 }
