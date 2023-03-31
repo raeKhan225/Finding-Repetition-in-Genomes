01cf631ab3f37c83bbe875b6574067ea94134a17
raeKhan225 <raeesahkhan225@gmail.com>
2023-03-31 15:56:10 +0100

    Now the progam can find microsats that may non A T C G values

--- Spike Work/hashTable/main.cpp
+++ Spike Work/hashTable/main.cpp
@@ -5,11 +5,38 @@
 
 using namespace std;
 
-class MicrosatFinder{
+class MicrosatFinder {
 public:
-    unordered_map<string, vector<int>> hashtable;
     /**
-     *
+     * The hashtable key is the pat that is in the microsat
+     * the vector stores the start and stop positions of where the micosat with that repeat occurs
+     */
+    unordered_map<string, vector<int>> hashtable;
+
+    /**
+     * Mismatch table
+     */
+    // used sting instead of array because of https://stackoverflow.com/questions/21946447/how-much-performance-difference-when-using-string-vs-char-array
+     unordered_map<char, string> misMatchTbl = { // should i order values in most to least likeyly??
+            {'A',"MRDHVWN"},
+            {'C',"YSBHVMN"},
+            {'G',"RKSBDVN"},
+            {'T',"YKWBDHN"},
+            {'M',"ACN"},
+            {'R',"GAN"},
+            {'Y',"TCN"},
+            {'K',"GTN"},
+            {'S',"GCN"},
+            {'W',"ATN"},
+            {'B',"GTCN"},
+            {'D',"GATN"},
+            {'H',"ACTN"},
+            {'V',"GCAN"},
+            {'N',"ACGTMRYKSWBDHV"},
+     };
+
+    /**
+     * FindMicrosat find the micosatellites in a DNA sequence
      * @param sequence
      * The sequence that the user inputs for microsatllite analysis
      * @param minLenRepeats
@@ -22,15 +49,18 @@ public:
      */
 
     // need to deal with mismatches
-    void findMicrosat(string sequence, int minLenRepeats, int maxLenRepeats, int minLenMicrosat){
+    void findMicrosat(const string& sequence, int minLenRepeats, int maxLenRepeats, int minLenMicrosat) {
         // Get length of the sequence
         int lenOfSequence = sequence.length();
+        string noMismatchRepeat = "";
+        int penaltyScore = 0;
 
         // Go through each nucleotide in sequence as microsat can start from any position
-        for(int nucleotidePos = 0; nucleotidePos <= lenOfSequence - minLenRepeats; nucleotidePos ++){
-            for(int lenOfRepeats = minLenRepeats; lenOfRepeats <= maxLenRepeats; lenOfRepeats ++){
+        for (int nucleotidePos = 0; nucleotidePos <= lenOfSequence - minLenRepeats; nucleotidePos++) {
+            //
+            for (int lenOfRepeats = minLenRepeats; lenOfRepeats <= maxLenRepeats; lenOfRepeats++) {
                 // check if end of sequence
-                if(nucleotidePos + lenOfRepeats >= lenOfSequence){
+                if (nucleotidePos + lenOfRepeats >= lenOfSequence) {
                     break;
                 }
 
@@ -38,17 +68,23 @@ public:
                 int startPos = nucleotidePos;
                 int endPos = -1; // set initial;y to -1 in case correct endpos hasn't been found
                 int lenThreshold = 0; // check to see if repeats pass the len threshold fo it to be considered a microsatellite
-                while(sequence.substr(nucleotidePos, lenOfRepeats) == sequence.substr(nucleotidePos + lenOfRepeats, lenOfRepeats)){
-                    // break at end of sequence
-                    if(nucleotidePos + lenOfRepeats == lenOfSequence){ break;} // MAY NOT NEED ?
-                    endPos = nucleotidePos + lenOfRepeats;
-                    lenThreshold ++;
+                while (compareRepeats(sequence.substr(nucleotidePos, lenOfRepeats),sequence.substr(nucleotidePos + lenOfRepeats, lenOfRepeats))){
+                    // assigned to the end of the second substring, *2 as nucleotidePos is the start pos of the first substring
+                    lenThreshold++;
                     nucleotidePos += lenOfRepeats;
+                    endPos = nucleotidePos + lenOfRepeats-1;
+
+                    // need to plus 1 if the last two sub stings of sequence, and they match
+                    if(endPos + 1 == lenOfSequence ){
+                        lenThreshold ++;
+                        break;
+                    }
                 }
 
                 // if repeats meet the threshold output the result
-                if(lenThreshold >= minLenMicrosat){
-                    cout << sequence.substr(nucleotidePos, lenOfRepeats) << "   StartPos: " << startPos << "   EndPos: " << endPos << endl;
+                if (lenThreshold >= minLenMicrosat) {
+                    cout << sequence.substr(nucleotidePos, lenOfRepeats) << "   StartPos: " << startPos << "   EndPos: "
+                         << endPos << endl;
                     addToHashtable(sequence.substr(nucleotidePos, lenOfRepeats), startPos, endPos);
                 }
             }
@@ -56,15 +92,42 @@ public:
         printHashTable();
     }
 
+    bool compareRepeats(string firstRepeat, string secondRepeat){ // both first and second repeat should be the same size
+        int lenFirstRepeat = firstRepeat.length();
+        // Goes through the bases in the first repeat
+        for(int base = 0; base < lenFirstRepeat; base ++){
+            // if the first base is equal to the second base i.e. A = A
+            if(firstRepeat[base] == secondRepeat[base]){
+                continue;
+            }
+            else{
+                string mismatches = misMatchTbl[firstRepeat[base]];
+                for(int i=0; i < mismatches.length(); i++){
+                    if(secondRepeat[base] == mismatches[i]){break;}
+                    else if (i == mismatches.length() -1){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Adds micosats to hashtable using the repeat as a key,
+     * @param repeat
+     * @param startPos
+     * @param endPos
+     */
     // need to find which sequence it is in
-    void addToHashtable(string repeat, int startPos, int endPos){
+    void addToHashtable(const string& repeat, int startPos, int endPos) {
 
         // If repeat is not in hashtable - if micosat hasn't been found already
-        if(hashtable.find(repeat) == hashtable.end()){
+        if (hashtable.find(repeat) == hashtable.end()) {
             hashtable[repeat] = {startPos, endPos};
         }
 
-        // if microsat has already been found in another position
+            // if microsat has already been found in another position
         else {
             vector<int> vec = hashtable[repeat];
             // Updating the vector with additional start and stop positions
@@ -73,6 +136,10 @@ public:
             hashtable[repeat] = vec;
         }
     }
+
+    /**
+     * Print out the hashtable for visual representation
+     */
 
     void printHashTable() {
         // Printing of Unordered_MAP
@@ -107,7 +174,8 @@ int main() {
     cout << "Enter the maximum length of repeats: ";
     cin >> maxLenRepeats;
 
-
+    cout << "Enter how many mismatches are allowed: ";
+    cin >> maxLenRepeats;
 
     MicrosatFinder microsatFinder;
     microsatFinder.findMicrosat(sequence, minLenRepeats, maxLenRepeats, minLenMicrosat);
