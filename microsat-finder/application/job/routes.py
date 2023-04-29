af268f184dad6bb4b8a6e5fe36e0741a7475a1a9
raeKhan225 <raeesahkhan225@gmail.com>
2023-04-29 16:00:56 +0100

    Offline version fixes

--- microsat-finder/application/job/routes.py
+++ microsat-finder/application/job/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_file
 # Importing flask app object
 from application import app
 from application.job.models import Job
@@ -20,3 +20,11 @@ def upload_file():
 @app.route("/view_current_jobs/", methods=['POST'])
 def get_current_jobs():
     return Job().getjobsfromdb()
+
+# This route is run when the user wants to download the microsat finder
+@app.route('/download')
+def download_file():
+    # path to the zip file to be downloaded
+    path = './static/offline_microsatfinder.zip'
+    # send the file to the user
+    return send_file(path, as_attachment=True)
